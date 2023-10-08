ff6c7dd48d89bb9af98db63009f3cc39eeb19c71
niccoloruberti <nikiruberti@gmail.com>
2023-10-08 15:03:37 +0200

    modificato messaggio di accesso in modo che venga mostrato solo se viene inviato il form

--- snack2.php
+++ snack2.php
@@ -1,30 +1,41 @@
 <?php
+$name = "";
+$mail = "";
+$age = "";
+$accessGranted = false; // Inizializziamo una variabile per verificare l'accesso
+
 if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
     $name = $_GET['name'];
     $mail = $_GET['mail'];
     $age = $_GET['age'];
+    
+    // Controllo le condizioni per l'accesso
+    if (strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
+        $accessGranted = true; // Impostiamo l'accesso a true se le condizioni sono soddisfatte
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form action="./snack2.php" method="GET">
-            <input type="text" name="name" placeholder="name">
-            <input type="mail" name="mail" placeholder="mail">
-            <input type="text" name="age" placeholder="age">
-            <button type="submit">Accedi</button>
-        </form>
-        <!-- controllo che il nome contenga più di 3 caratteri, che la mail contena '@' e '.' e che l'età sia un numero -->
-        <?php if ( strlen($name) > 3 && str_contains($mail, '.') && str_contains($mail, '@') && is_numeric($age)) {?>
-            <p><strong>Accesso Riuscito!</strong></p>
-        <?php } else {?>
-            <p><strong>Accesso Negato!</strong></p>
-        <?php }?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./snack2.php" method="GET">
+        <input type="text" name="name" placeholder="name">
+        <input type="email" name="mail" placeholder="mail">
+        <input type="text" name="age" placeholder="age">
+        <button type="submit">Accedi</button>
+    </form>
+    
+    <!-- Mostrare il messaggio solo se l'accesso è stato concesso -->
+    <?php if ($accessGranted) {?>
+        <p><strong>Accesso Riuscito!</strong></p>
+    <?php } else if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {?>
+        <p><strong>Accesso Negato!</strong></p>
+    <?php }?>
+</body>
 </html>
